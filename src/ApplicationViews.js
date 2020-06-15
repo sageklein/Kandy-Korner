2558cd22f268f2863ee0c16b6372325dc00f5308
Sage Klein <sageklein@gmail.com>
2020-06-15 10:29:24 -0500

    added application views and NavBar is showing.

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,36 +1,82 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
+import React from "react";
+import ProductList from "./Product/ProductList";
+import ProductForm from "./Product/ProductForm";
+import ProductDetails from "./Product/ProductDetails";
+import EmployeeList from "./Employee/EmployeeList";
+import LocationList from "./Location/LocationList";
+import Login from "./Login/Login";
 
+const ApplicationViews = () => {
+	const isAuthenticated = () =>
+		sessionStorage.getItem("credentials") !== null;
+	return (
+		<React.Fragment>
+			<Route path="/login" component={Login} />
 
-class NavBar extends Component {
-	render() {
-		return (
-			<nav className="navBarWrapper">
-				<ul className="navBar">
-					<li className="navItem">
-						<Link className="navLink" to="/">
-							Home
-						</Link>
-					</li>
-					<li className="navItem">
-						<Link className="navLink" to="/locations">
-							Locations
-						</Link>
-					</li>
-					<li className="navItem">
-						<Link className="navLink" to="/products">
-							Messages
-						</Link>
-					</li>
-					{/* <li className="navItem">
-						<Link className="navLink" to="">
-							
-						</Link>
-					</li> */}
-				</ul>
-			</nav>
-		);
-	}
-}
+			<Route
+				exact
+				path="/products"
+				render={(props) => {
+					if (isAuthenticated()) {
+						return <ProductList {...props} />;
+					} else {
+						return <Redirect to="/login" />;
+					}
+				}}
+			/>
+			<Route
+				path="/products/new"
+				render={(props) => {
+					return <ProductForm {...props} />;
+				}}
+			/>
+			<Route
+				path="/products/:productId(\d+)"
+				render={(props) => {
+					return (
+						<ProductDetails
+							productId={parseInt(props.match.params.productId)}
+						/>
+					);
+				}}
+			/>
 
-export default NavBar;
+			<Route
+				exact
+				path="/employees"
+				render={(props) => {
+					if (isAuthenticated()) {
+						return <EmployeeList />;
+					} else {
+						return <Redirect to="/login" />;
+					}
+				}}
+			/>
+			{/* <Route
+				path="/employees/:employeeId(\d+)"
+				render={(props) => {
+					return (
+						<EmployeeDetails
+							employeeId={parseInt(props.match.params.employeeId)}
+						/>
+					);
+				}}
+			/> */}
+
+			<Route
+				exact
+				path="/locations"
+				render={(props) => {
+					if (isAuthenticated()) {
+						return <LocationList />;
+					} else {
+						return <Redirect to="/login" />;
+					}
+				}}
+			/>
+		</React.Fragment>
+	);
+};
+
+export default ApplicationViews;
